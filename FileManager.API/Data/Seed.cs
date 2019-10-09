c7a149af0403ca13248dc674cda771881c7637a3
Phil Mosely <moselyphil@gmail.com>
2019-10-09 12:57:12 -0400

    Folder addremove in progress

--- FileManager.API/Data/Seed.cs
+++ FileManager.API/Data/Seed.cs
@@ -82,21 +82,9 @@ namespace FileManager.API.Data
                     SubFolderName = "Admin Folder",
                     DateCreated = DateTime.Now,
                     DateModified = DateTime.Now,
-                    FolderData = "[{\"id\": \"2\",\"parentid\": \"1\",\"text\": \"Hot Chocolate\",\"value\": \"$2.3\"},{\"id\": \"3\",\"parentid\": \"1\",\"text\": \"Peppermint Hot Chocolate\",\"value\": \"$2.3\"},{\"id\": \"4\",\"parentid\": \"1\",\"text\": \"Salted Caramel Hot Chocolate\",\"value\": \"$2.3\"},{\"id\": \"5\",\"parentid\": \"1\",\"text\": \"White Hot Chocolate\",\"value\": \"$2.3\"},{\"text\": \"Chocolate Beverage\",\"id\": \"1\",\"parentid\": \"-1\",\"value\": \"$2.3\"},{\"id\": \"6\",\"text\": \"Espresso Beverage\",\"parentid\": \"-1\",\"value\": \"$2.3\"},{\"id\": \"7\",\"parentid\": \"6\",\"text\": \"Caffe Americano\",\"value\": \"$2.3\"}]"
+                    FolderData = "[{\"id\": \"2\",\"parentid\": \"1\",\"text\": \"Hot Chocolate\",\"value\": \"$2.3\"},{\"id\": \"3\",\"parentid\": \"1\",\"text\": \"Peppermint Hot Chocolate\",\"value\": \"$2.3\"},{\"id\": \"4\",\"parentid\": \"1\",\"text\": \"Salted Caramel Hot Chocolate\",\"value\": \"$2.3\"},{\"id\": \"5\",\"parentid\": \"1\",\"text\": \"White Hot Chocolate\",\"value\": \"$2.3\"},{\"text\": \"Chocolate Beverage\",\"id\": \"1\",\"parentid\": \"0\",\"value\": \"$2.3\"},{\"id\": \"6\",\"text\": \"Espresso Beverage\",\"parentid\": \"0\",\"value\": \"$2.3\"},{\"id\": \"7\",\"parentid\": \"6\",\"text\": \"Caffe Americano\",\"value\": \"$2.3\"}]"
                 };
                 context.FileManagerAdmin.Add(fmAdmin);
-
-                var file = new File {
-                    FMAdmin = fmAdmin,
-                    FileName = "user.png",
-                    Ext = "png",
-                    Url = "./assets/img/user.png",
-                    Size = 12000,
-                    DateCreated = DateTime.Now,
-                    DateModified = DateTime.Now,
-                    NodeId = 2
-                };
-                context.Files.Add(file);
 
             }
 
