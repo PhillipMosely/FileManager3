b0b1b6e39497ae6a1ea638feb142612c8ee40a90
Phil Mosely <moselyphil@gmail.com>
2019-10-18 16:56:00 -0400

    Removed 5000 string length

--- FileManager.API/Models/Company.cs
+++ FileManager.API/Models/Company.cs
@@ -12,7 +12,6 @@ namespace FileManager.API.Models
         public string WebsiteUrl { get; set; }       
         public string LogoUrl { get; set; }
         public string OverallStyleConfig { get; set; }
-        [StringLength(5000)]
         public string ComponentConfig { get; set;}
     }
 }
